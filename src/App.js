afb2da8b9d9b9a3302b4add97f166e2cfccc0a7a
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-05-21 01:50:27 +0400

    feat: get ready general structure for working on functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import remove from "../src/images/icon-remove.svg";
+import poto from "./images/photosnap.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchbar">
+        <div className="searchItems">
+          <span>frontend</span>
+          <div className="remove">
+            <img src={remove} alt="remove icon" />
+          </div>
+        </div>
+        <span>clear</span>
+      </div>
+      <div className="jobcontainer">
+        <img className="image" src={poto} />
+        <div>
+          <div className="description">
+            <h2>Photosnap</h2>
+            <span>NEW!</span>
+            <span>FEATURED</span>
+          </div>
+          <h1>1</h1>
+          <div className="text-box">
+            <span>1d ago</span>
+            <span>Full Time</span>
+            <span>USA only</span>
+          </div>
+        </div>
+        <div className="skillbox">
+          <span>css</span>
+          <span>Frontend</span>
+          <span>JavaScript</span>
+          <span>Junior</span>
+        </div>
+      </div>
+      <div className="jobcontainer">
+        <img className="image" src={poto} alt="logo" />
+        <div>
+          <div className="description">
+            <h2>Photosnap</h2>
+            <span>NEW!</span>
+            <span>FEATURED</span>
+          </div>
+          <h1>2</h1>
+          <div className="text-box">
+            <span>1d ago</span>
+            <span>Full Time</span>
+            <span>USA only</span>
+          </div>
+        </div>
+        <div className="skillbox">
+          <span>css</span>
+          <span>Frontend</span>
+          <span>JavaScript</span>
+          <span>Junior</span>
+        </div>
+      </div>
+      <div className="jobcontainer">
+        <img className="image" src={poto} />
+        <div>
+          <div className="description">
+            <h2>Photosnap</h2>
+            <span>NEW!</span>
+            <span>FEATURED</span>
+          </div>
+          <h1>3</h1>
+          <div className="text-box">
+            <span>1d ago</span>
+            <span>Full Time</span>
+            <span>USA only</span>
+          </div>
+        </div>
+        <div className="skillbox">
+          <span>css</span>
+          <span>Frontend</span>
+          <span>JavaScript</span>
+          <span>Junior</span>
+        </div>
+      </div>
     </div>
   );
 }
